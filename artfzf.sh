fe51de89efe9e4d48ca11fbeff4ff9b299cd3c71
DemonKingSwarn <rockingswarn@gmail.com>
2022-07-27 18:48:38 +0530

    Chore: script update

--- artfzf.sh
+++ artfzf.sh
@@ -1,30 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 url="https://artgare.com"
 download_dir="$HOME/Downloads"
 
-download() {
-    aria2c --dir="${download_dir}" "$1"
-}
+selected=$(curl -s "$url" -A "uwu" | tr '<|>' '\n' | sed -nE 's_a href="(.*model/[^"]*)"$_\1_p' | fzf -d'/' --with-nth 5)
 
-main() {
-    resp=$(curl -s "$1" -A "uwu" | grep -Eo "https://artgare.com/free-3d-model/[a-zA-Z0-9%-]*/" | uniq)
-    selected=$(printf "%s\n" "$resp" | fzf)
-    
-    [[ -z "$selected" ]] && printf "%s\n" "[!] Nothing to download" && exit 1
-    
-    req=$(curl -s "$selected" | grep -Eo "https://cdn.discordapp.com/attachments/[0-9]{18}/[0-9]{18}/[a-zA-Z0-9_-]*.zip")
-    image=$(curl -s "$selected" | grep -Eo "https://cdn.discordapp.com/attachments/[0-9]{18}/[0-9]{18}/[a-zA-Z0-9_-]*.webp" | head -n 1)
-    
-    w3m "$image"
-    
-    printf "%s" "Are you sure you want to download (Y/N): " && read -r query
-    
-    if [[ "$query" == "Y" || "$query" == "y" ]] ; then
-        download "$req"
-    else
-        printf "%s\n" "Exiting..."
-    fi
-}
+[[ -z "$selected" ]] && printf "%s\n" "[!] Nothing to download" && exit 1
 
-main "$url"
+req=$(curl -s "$selected" | tr '<|>' '\n' | sed -nE 's_a href="(.*\.zip[^"]*)".*_\1_p; s_.*content="(.*\.webp[^"]*)".*_\1_p')
+
+w3m "$(printf "%s" "$req" | head -1 )"
+
+printf "%s" "Are you sure you want to download (Y/N): " && read -r query
+
+if [ "$query" = "Y" ] || ["$query" = "y" ]; then
+    aria2c --dir="${download_dir}" "$(printf "%s" "$req" | tail -1)"
+else
+    printf "%s\n" "Exiting..."
+fi
